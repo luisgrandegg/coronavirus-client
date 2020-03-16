f90c69249eaf911e2d5b4991a5f1a81ab5d6fabb
Santi Elizondo <santiago.elizondo@hyve.net>
2020-03-16 21:08:13 +0100

    Add section component.

---
+++ src/components/Section.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export interface ISectionProps {
+    aside: JSX.Element,
+    content: JSX.Element
+};
+
+export const Section: React.FunctionComponent<ISectionProps> = (props: ISectionProps): JSX.Element => (
+    <div className="section-wrapper">
+        <div className="section-wrapper__content">{props.content}</div>
+        <div className="section-wrapper__aside">{props.aside}</div>
+    </div>
+);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,64 +5,72 @@ import { Link as RouterLink } from 'react-router-dom';
 
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
+import { Section } from '../components/Section';
 import { Routes } from '../router/Routes';
 
 export const Home: React.FunctionComponent = (): JSX.Element => {
     const { t } = useTranslation();
+
+    const Content = (): JSX.Element => (
+        <div>
+            <section className="home__section">
+                <header className="home__section-header">
+                    <h2 className="home__section-title">{t('home.what-is.title')}</h2>
+                </header>
+                <p dangerouslySetInnerHTML={{ __html: t('home.what-is.first-paragraph') }} />
+                <p dangerouslySetInnerHTML={{ __html: t('home.what-is.second-paragraph') }} />
+            </section>
+            <section className="home__section">
+                <header className="home__section-header">
+                    <h2 className="home__section-title">{t('home.what-is-not.title')}</h2>
+                </header>
+                <p dangerouslySetInnerHTML={{ __html: t('home.what-is-not.first-paragraph') }} />
+                <p dangerouslySetInnerHTML={{ __html: t('home.what-is-not.second-paragraph') }} />
+            </section>
+            <section className="home__section home__section--cta">
+                <header className="home__section-header">
+                    <h2 className="home__section-title">{t('home.pacient.title')}</h2>
+                </header>
+                <p>{t('home.pacient.first-paragraph')}</p>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    component={RouterLink}
+                    to={Routes.REGISTER_PATIENT}
+                >{t('home.pacient.cta')}</Button>
+            </section>
+            <section className="home__section home__section--cta">
+                <header className="home__section-header">
+                    <h2 className="home__section-title">{t('home.doctor.title')}</h2>
+                </header>
+                <p>{t('home.doctor.first-paragraph')}</p>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    component={RouterLink}
+                    to={Routes.REGISTER_DOCTOR}
+                >{t('home.doctor.cta')}</Button>
+            </section>
+        </div>
+    );
+
+    const Aside = (): JSX.Element => (
+        <div className="home__drawing">
+            <img
+                src="/images/citamedicaencasa.png"
+                alt={t('home.drawing.alt')}
+            />
+        </div>
+    );
 
     return (
         <>
             <Header />
             <main className="main home">
                 <div className="container">
-                    <div className="section">
-                        <div className="content">
-                            <section className="home__section">
-                                <header className="home__section-header">
-                                    <h2 className="home__section-title">{t('home.what-is.title')}</h2>
-                                </header>
-                                <p dangerouslySetInnerHTML={{ __html: t('home.what-is.first-paragraph') }} />
-                                <p dangerouslySetInnerHTML={{ __html: t('home.what-is.second-paragraph') }} />
-                            </section>
-                            <section className="home__section">
-                                <header className="home__section-header">
-                                    <h2 className="home__section-title">{t('home.what-is-not.title')}</h2>
-                                </header>
-                                <p dangerouslySetInnerHTML={{ __html: t('home.what-is-not.first-paragraph') }} />
-                                <p dangerouslySetInnerHTML={{ __html: t('home.what-is-not.second-paragraph') }} />
-                            </section>
-                            <section className="home__section home__section--cta">
-                                <header className="home__section-header">
-                                    <h2 className="home__section-title">{t('home.pacient.title')}</h2>
-                                </header>
-                                <p>{t('home.pacient.first-paragraph')}</p>
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                    component={RouterLink}
-                                    to={Routes.REGISTER_PATIENT}
-                                >{t('home.pacient.cta')}</Button>
-                            </section>
-                            <section className="home__section home__section--cta">
-                                <header className="home__section-header">
-                                    <h2 className="home__section-title">{t('home.doctor.title')}</h2>
-                                </header>
-                                <p>{t('home.doctor.first-paragraph')}</p>
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                    component={RouterLink}
-                                    to={Routes.REGISTER_DOCTOR}
-                                >{t('home.doctor.cta')}</Button>
-                            </section>
-                        </div>
-                        <div className="home__drawing">
-                            <img
-                                src="/images/citamedicaencasa.png"
-                                alt={t('home.drawing.alt')}
-                            />
-                        </div>
-                    </div>
+                    <Section
+                        aside={<Aside />}
+                        content={<Content />} />
                 </div>
             </main>
             <Footer />
