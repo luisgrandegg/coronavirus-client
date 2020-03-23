66c92d0a122fae82e736b7225048f8fcfdc88f4d
Santi Elizondo <santiago.elizondo@hyve.net>
2020-03-23 21:24:25 +0100

    feat: add two step form to create inquiries.

--- src/theme.js
+++ src/theme.js
@@ -1,10 +1,18 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
 export default createMuiTheme({
+<<<<<<< HEAD
   palette: {
     primary: {
       main: "#20ABD6"
     }
+=======
+  typography: {
+    fontFamily: [
+      'Open Sans',
+      'sans-serif',
+    ].join(','),
+>>>>>>> feat: add two step form to create inquiries.
   },
   overrides: {
     MuiButton: {
