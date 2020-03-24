bc9fb39636d61bfe01a9edc18089281180949a26
luisgrandegg <luisgrande@packlink.com>
2020-03-24 19:58:14 +0100

    fix: 🐛 error in checkbox component

--- src/components/Form/Checkbox.tsx
+++ src/components/Form/Checkbox.tsx
@@ -4,15 +4,15 @@ import MaterialCheckbox, { CheckboxProps } from '@material-ui/core/Checkbox'
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormHelperText from '@material-ui/core/FormHelperText';
 
-export type ICheckboxProps = CheckboxProps & {
+export interface ICheckboxProps extends Omit<CheckboxProps, 'form'> {
     label: string;
     field: FieldInputProps<string> | FieldInputProps<string[]>;
-    formikProps: FormikProps<any>;
+    form: FormikProps<any>;
 };
 
 export const Checkbox: React.FunctionComponent<ICheckboxProps> = (props: ICheckboxProps) => {
-    const { label, name, field, formikProps, ...other } = props;
-    const { touched, errors } = formikProps;
+    const { label, name, field, form, ...other } = props;
+    const { touched, errors } = form;
     const errorText = getIn(errors, field.name);
     const touchedVal = getIn(touched, field.name);
     const hasError = touchedVal && errorText !== undefined;
