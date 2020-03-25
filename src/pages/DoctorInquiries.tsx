9f5fbfe17fe8cf3af0548422fe03fef22a4ecc3b
luisgrandegg <luisgrande@packlink.com>
2020-03-25 20:11:59 +0100

    refactor: 💡 inquiryList and inquiryCard

--- src/pages/DoctorInquiries.tsx
+++ src/pages/DoctorInquiries.tsx
@@ -9,8 +9,12 @@ import { InquiryListParams } from '../dto/InquiryListParams';
 import { useSelector } from 'react-redux';
 import { getAuth } from '../store/selectors/status';
 import { Auth } from '../entities/Auth';
+import { useHistory } from 'react-router-dom';
+import { Inquiry } from '../entities/Inquiry';
+import { Routes } from '../router/Routes';
 
 export const DoctorInquiries: React.FunctionComponent = (): JSX.Element => {
+    const history = useHistory();
     const auth = useSelector(getAuth);
 
     const inquiryListParams: InquiryListParams = InquiryListParams.deserialize({
@@ -18,6 +22,12 @@ export const DoctorInquiries: React.FunctionComponent = (): JSX.Element => {
         flagged: false,
         doctorId: (auth as Auth).userId
     });
+
+    const handleAttendEvent = (inquiry: Inquiry): void => {
+        if (!inquiry.attended) {
+            history.push(Routes.DOCTOR_DASHBOARD);
+        }
+    }
 
     return(
         <>
@@ -27,7 +37,7 @@ export const DoctorInquiries: React.FunctionComponent = (): JSX.Element => {
             <main className="main doctor-inquiries">
                 <div className="container">
                     <BackHome />
-                    <InquiryList inquiryListParams={inquiryListParams}/>
+                    <InquiryList inquiryListParams={inquiryListParams} onAttendEvent={handleAttendEvent}/>
                 </div>
             </main>
             <Footer/>
--- src/theme.js
+++ src/theme.js
@@ -1,32 +1,30 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
 export default createMuiTheme({
-<<<<<<< HEAD
-  palette: {
-    primary: {
-      main: "#20ABD6"
-    }
-=======
-  typography: {
-    fontFamily: [
-      'Open Sans',
-      'sans-serif',
-    ].join(','),
->>>>>>> feat: add two step form to create inquiries.
-  },
-  overrides: {
-    MuiButton: {
-      containedPrimary: {
-        color: "white"
-      }
+    palette: {
+        primary: {
+        main: "#20ABD6"
+        }
     },
-    MuiChip: {
-      colorPrimary: {
-        color: "white"
-      },
-      deleteIconColorPrimary: {
-        color: "white"
-      }
-    }
-  },
+    typography: {
+        fontFamily: [
+        'Open Sans',
+        'sans-serif',
+        ].join(','),
+    },
+    overrides: {
+        MuiButton: {
+            containedPrimary: {
+                color: "white"
+            }
+        },
+        MuiChip: {
+            colorPrimary: {
+                color: "white"
+            },
+            deleteIconColorPrimary: {
+                color: "white"
+            }
+        }
+    },
 });
