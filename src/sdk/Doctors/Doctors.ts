27ab5207363e0f2b4210c6110cf829ffbac9729a
luisgrandegg <luisgrande@packlink.com>
2020-03-26 07:42:49 +0100

    feat: 🎸 add doctor comments

--- src/sdk/Doctors/Doctors.ts
+++ src/sdk/Doctors/Doctors.ts
@@ -13,4 +13,9 @@ export class Doctors {
         return this.apiClient.get<IDoctorApiResponse[]>('/doctors', doctorListParams)
             .then((response: AxiosResponse<IDoctorApiResponse[]>): Doctor[] => response.data.map(Doctor.createFromResponse));
     }
+
+    async comment(id: string, comment: string): Promise<Doctor> {
+        return this.apiClient.post<IDoctorApiResponse>(`/doctors/${id}/comment`, { comment })
+            .then((response: AxiosResponse<IDoctorApiResponse>): Doctor => Doctor.createFromResponse(response.data));
+    }
 }
